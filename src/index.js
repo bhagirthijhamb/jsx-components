d73a0689b4dcc71768f9699b3bc5b07671a50a23
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-14 23:13:08 -0500

    added JSX basics and started Component basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function getButtonText(){
+//   return 'Click on me!';
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a react component
+const App = () => {
+  // const buttonText = 'Click Me!'
+  // const btnText = 1234;
+  // const btnText = ['Hi', 'there'];
+  // const btnText = [10, 20];
+  // const btnText = {text: 'Click me'};
+  // const style = { backgroundColor:  'blue', color: 'white' };
+  return (
+    // <div>
+      // {/* <label className="label" htmlFor="name">Enter name:</label>
+      // <input if="name" type="text" /> */}
+      // {/* <button style={{ backgroundColor:  'blue', color: 'white'}}>Submit</button> */}
+      // {/* <button style={{ backgroundColor:  'blue', color: 'white'}}>{buttonText}</button> */}
+      // {/* <button style={{ backgroundColor:  'blue', color: 'white'}}>{getButtonText()}</button> */}
+      // {/* <button style={{ backgroundColor:  'blue', color: 'white'}}>{btnText}</button> */}
+      // {/* <button style={style}>{btnText.text}</button> */}
+    // </div>  
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          {/* <img alt="avatar" src={faker.image.avatar()} /> */}
+          <img alt="avatar" src={faker.image.cats()} />
+        </a>
+        <div className="content"> 
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today st 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'))
